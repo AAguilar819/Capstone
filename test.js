40edd4d809ef93b82d04bac41c934c3a46b83d5c
Abraham Aguilar <aaguilar819@west-mec.org>
2018-04-25 18:13:09 -0700

    File Update
    +Added the form
    +Added basic styles to the form
    +Attempted to create JS code for the form

--- test.js
+++ test.js
@@ -1,3 +1,33 @@
 "use strict";
 
 // Testing file to create some basic JS code
+
+// var fnBox = document.getElementById("fnBox");
+var lnBox = document.getElementById("lnBox");
+var mailBox = document.getElementById("mailBox");
+var phoneBox = document.getElementById("phoneBox");
+var reasonBox = document.getElementById("reasonBox");
+var productBox = document.getElementById("productBox");
+
+window.addEventListener("load", function() {
+    validityTest();
+
+    fnBox.onblur = calcCart;
+    lnBox.onblur = calcCart;
+    mailBox.onblur = calcCart;
+    phoneBox.onblur = calcCart;
+    reasonBox.onblur = calcCart;
+    productBox.onclick = calcCart;
+})
+
+function validityTest() {
+    if (document.getElementById("fnBox").validity = false) {
+        fnBox.setCustomValidity("Please insert your First Name");
+    } else {
+        fnBox.setCustomValidity("");
+    }
+}
+
+function calcCart() {
+    var packages = document.getElementById("productBox")
+}
