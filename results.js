a21249415716efd58f6e79b7ca9b8b5ab917462e
Abraham Aguilar <aaguilar819@west-mec.org>
2018-04-27 17:49:40 -0700

    Form validated page
    +Added the necessary files to create the page sent to by the form
    +Adjusted stock and test.js to work with the added files

--- results.js
+++ results.js
@@ -1,55 +1,27 @@
 "use strict";
 
-// Testing file to create some basic JS code
+window.addEventListener("load", function() {
+    calcCart();
+    writeSessionValues();
+})
 
-// var fnBox = document.getElementById("fnBox");
-// var lnBox = document.getElementById("lnBox");
-// var mailBox = document.getElementById("mailBox");
-// var phoneBox = document.getElementById("phoneBox");
-// var productBox = document.getElementById("productBox");
-// var reasonBox = document.getElementById("reasonBox");
 var generalContents = ["4 MREs", "4 Road Flare", "2 Flint and Steel", "4 Blanket", "4 Sets of 4 Braces", "3 Flashlight", "2 First Aid Kits", "2 Water Purifiers", "3 Sets of Clothing", "10 Water Bottles"];
 var droughtContents = ["4 MREs", "4 Road Flare", "2 Flint and Steel", "4 Blanket", "4 Sets of 4 Braces", "3 Flashlight", "2 First Aid Kits", "4 Water Purifiers", "3 Sets of Clothing", "20 Water Bottles", "4 Medium-Duty Tarps"];
 var floodContents = ["4 MREs", "4 Road Flare", "2 Flint and Steel", "4 Blanket", "4 Sets of 4 Braces", "3 Flashlight", "2 First Aid Kits", "4 Water Purifiers", "3 Sets of Clothing", "20 Water Bottles", "Self-Inflating Life Raft", "6 Life Jackets"];
 var earthquakeContents = ["4 MREs", "4 Road Flare", "2 Flint and Steel", "4 Blanket", "4 Sets of 4 Braces", "3 Flashlight", "2 First Aid Kits", "4 Water Purifiers", "3 Sets of Clothing", "20 Water Bottles", "Building Supplies", "Propane Tank"];
 
-sessionStorage.setItem("confName", document.getElementById("fnBox") + " " + document.getElementById("lnBox"));
-sessionStorage.setItem("confMail", document.getElementById("mailBox"));
-sessionStorage.setItem("confPhone", document.getElementById("phoneBox"));
+var packages = sessionStorage.getItem("confProduct");
 
-window.addEventListener("load", function() {
-    validityTest();
-    calcCart();
-
-    // fnBox.onblur = calcCart;
-    // lnBox.onblur = calcCart;
-    // mailBox.onblur = calcCart;
-    // phoneBox.onblur = calcCart;
-    // reasonBox.onblur = calcCart;
-    productBox.onchange = calcCart;
-})
-
-// The below funciton needs some bugfixing
-function validityTest() {
-    if (document.getElementById("fnBox").length = 0) {
-        document.getElementById("fnBox").setCustomValidity("Please insert your First Name");
-    } else {
-        document.getElementById("fnBox").setCustomValidity("");
-    }
-}
-// sessionBox.options[sessionBox.selectedIndex].textContent
 function calcCart() {
     // var cost = document.getElementById("productBox").options[document.getElementById("productBox").selectedIndex].value;
-
-    var packages = document.getElementById("productBox").options[document.getElementById("productBox").selectedIndex];
-    sessionStorage.setItem("confProduct", packages.value);
-    if (packages.value == 275) {
+    //  var packages = sessionStorage.getItem("confProduct");
+    if (packages == 275) {
         displayContents(generalContents);
-    } else if (packages.value == 750) {
+    } else if (packages == 750) {
         displayContents(droughtContents);
-    } else if (packages.value == 1750) {
+    } else if (packages == 1750) {
         displayContents(floodContents);
-    } else if (packages.value == 3750) {
+    } else if (packages == 3750) {
         displayContents(earthquakeContents);
     }
 }
@@ -63,8 +35,21 @@ function displayContents(array) {
         if (document.getElementsByTagName("span")[i].textContent != array[i]) {
             document.getElementsByTagName("span")[i].style.padding = "0px";
             document.getElementsByTagName("span")[i].style.border = "0px";
-        }
+            document.getElementsByTagName("span")[i].style.backgroundColor = "none";
+        } // else if (document.getElementsByTagName("span")["id"].length != 0) {
+        // document.getElementsByTagName("span")[i].style.padding = "0px";
+        // document.getElementsByTagName("span")[i].style.border = "0px";
+        // document.getElementsByTagName("span")[i].style.backgroundColor = "none";
+        // }
 
     }
     // This allows for less loading as the function will replace the need for nearly identical blocks of code in calculating the contents.
+}
+
+function writeSessionValues() {
+    document.getElementById("name").textContent = sessionStorage.getItem("confName").value;
+    document.getElementById("email").textContent = sessionStorage.getItem("confMail").value;
+    document.getElementById("phone").textContent = sessionStorage.getItem("confPhone").value;
+    document.getElementById("product").textContent = sessionStorage.getItem("confProduct").value;
+    // This displays all the stored variables into the browser.
 }
