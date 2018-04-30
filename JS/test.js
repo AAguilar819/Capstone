4fcfb59007f17a6b0ad2cb9e14fc743f76fa0c2c
Unknown <ZMiranda620@west-mec.org>
2018-04-30 16:22:12 -0700

    page update
    Added more bootstrap elements to the page to display all paragraph around images as well as effected the navigation bar to speace out between each link.

--- JS/test.js
+++ JS/test.js
@@ -1,12 +1,12 @@
-"use strict";
+// "use strict";
 
-// Testing file to create some basic JS code
+// // Testing file to create some basic JS code
 
-$("document").ready(function() {
-    if ($("p").css("font-size", "default").value = true) {
-        $("p").css("border", "3px solid aquamarine");
-        alert("Styles are needed with font size")
-    } else {
-        alert("There are necessary styles set into the paragraphs");
-    }
-})
+// $("document").ready(function() {
+//     if ($("p").css("font-size", "default").value = true) {
+//         $("p").css("border", "3px solid aquamarine");
+//         alert("Styles are needed with font size")
+//     } else {
+//         alert("There are necessary styles set into the paragraphs");
+//     }
+// })
