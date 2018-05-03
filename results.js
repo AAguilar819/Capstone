9c466fd494a9346d7ff8928068d6baa5b6183acd
Abraham Aguilar <aaguilar819@west-mec.org>
2018-05-02 18:08:06 -0700

    Credit card Information
    +Copied some core code to get a credit card information portion of the form up.
    +started debugging the code and converting it into a usabel form for this project.

--- results.js
+++ results.js
@@ -3,6 +3,7 @@
 window.addEventListener("load", function() {
     calcCart();
     writeSessionValues();
+    runSubmit();
 })
 
 var generalContents = ["4 MREs", "4 Road Flare", "2 Flint and Steel", "4 Blanket", "4 Sets of 4 Braces", "3 Flashlight", "2 First Aid Kits", "2 Water Purifiers", "3 Sets of Clothing", "10 Water Bottles"];
@@ -62,4 +63,94 @@ function writeSessionValues() {
     document.getElementById("phone").textContent = phone;
     document.getElementById("product").textContent = nameProduct(packages);
     // This displays all the stored variables into the browser.
+}
+
+function runSubmit() {
+    validateCredit();
+    validateNumber();
+    validateMonth();
+    validateYear();
+    validateCVC();
+}
+
+function validateCVC() {
+    var cardCVC = document.getElementById("cvc");
+    var creditCard = document.querySelector('input[name="credit"]:checked').value;
+    if (cardCVC.validity.valueMissing) {
+        cardCVC.setCustomValidity("Enter your CVC number.");
+    } else if ((creditCard === "amex") && (/^\d{4}$/.test(cardCVC.value) === false)) {
+        cardCVC.setCustomValidity("Enter a 4-digit CVC number.");
+    } else if ((creditCard !== "amex") && (/^\d{3}$/.test(cardCVC.value) === false)) {
+        cardCVC.setCustomValidity("Enter a 3-digit CVC number.");
+    } else {
+        cardCVC.setCustomValidity("");
+    };
+}
+
+function validateMonth() {
+    var cardMonth = document.getElementById("expMonth");
+    if (cardMonth.selectedIndex === 0) {
+        cardMonth.setCustomValidity("Select the expiration month.");
+    } else {
+        cardMonth.setCustomValidity("");
+    };
+}
+
+function validateYear() {
+    var cardYear = document.getElementById("expYear");
+    if (cardYear.selectedIndex === 0) {
+        cardYear.setCustomValidity("Select the expiration year.");
+    } else {
+        cardYear.setCustomValidity("");
+    };
+}
+
+function validateNumber() {
+    var cardNumber = document.getElementById("cardNumber");
+    if (cardNumber.validity.valueMissing) {
+        cardNumber.setCustomValidity("Enter your card number.");
+    } else if (cardNumber.validity.patternMismatch) {
+        cardNumber.setCustomValidity("Enter a valid credit card number.");
+    } else if (luhn(cardNumber.value) === false) {
+        cardNumber.setCustomValidity("Enter a legitimate credit card number.");
+    } else {
+        cardNumber.setCustomValidity("");
+    };
+}
+
+function validateCredit() {
+    var creditCard = document.forms.payment.elements.credit[0];
+    if (creditCard.validity.valueMissing) {
+        creditCard.setCustomValidity("Select your credit card.");
+    } else {
+        creditCard.setCustomValidity("");
+    };
+}
+
+function sumDigits(numStr) {
+    var digitTotal = 0;
+    for (var i = 0; i < numStr.length; i++) {
+        digitTotal += parseInt(numStr.charAt(i));
+    }
+    return digitTotal;
+}
+
+function luhn(idNum) {
+    var string1 = "";
+    var string2 = "";
+
+    // Retrieves the odd numbered digits
+
+    for (var i = idNum.length - 1; i >= 0; i -= 2) {
+        string1 += idNum.charAt(i);
+    }
+
+    // Retrieves the even numbered digits and double them
+
+    for (var i = idNum.length - 2; i >= 0; i -= 2) {
+        string2 += 2 * idNum.charAt(i);
+    }
+
+    // Return whether the sum of the digits is divisible by 10
+    return sumDigits(string1 + string2) % 10 === 0;
 }
